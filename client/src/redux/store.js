231783605605d22001728ddf37570ee97ffe6370
myrzagul-A <112957802+myrzagul-A@users.noreply.github.com>
2022-09-21 11:55:56 -0400

    Updated store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
+import singleNewsSlice from './features/news/singleNewsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    news: singleNewsSlice,
   },
 });
+
+
+
